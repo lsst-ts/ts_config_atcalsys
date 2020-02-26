f7277b416163eb8e0b1a5dd5469710fdd3ba8140
Russell Owen <rowen@uw.edu>
2020-02-25 16:27:17 -0800

    Test config files.

---
+++ tests/test_config_files.py
@@ -0,0 +1,47 @@
+# This file is part of ts_config_atcalsys.
+#
+# Developed for the LSST Telescope and Site Systems.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import pathlib
+import unittest
+
+from lsst.ts import salobj
+
+
+class ConfigTestCase(salobj.BaseConfigTestCase, unittest.TestCase):
+    def setUp(self):
+        self.config_package_root = pathlib.Path(__file__).parents[1]
+
+    def test_ATMonochromator(self):
+        self.check_standard_config_files(
+            sal_name="ATMonochromator",
+            module_name="lsst.ts.monochromator",
+            config_package_root=self.config_package_root,
+        )
+
+    def test_ATWhiteLight(self):
+        # Use env vars to find ts_ATWhiteLightSource because it uses
+        # packages that are not in the standard development Docker image.
+        self.check_standard_config_files(
+            sal_name="ATWhiteLight",
+            package_name="ts_ATWhiteLightSource",
+            schema_subpath="schema/whitelight.yaml",
+            config_package_root=self.config_package_root,
+        )
